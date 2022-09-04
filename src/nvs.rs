99d84155af7edcc750db81343a98634ae51cb6ee
ivmarkov <ivan.markov@gmail.com>
2022-09-04 16:50:03 +0300

    bugfixing

--- src/nvs.rs
+++ src/nvs.rs
@@ -343,7 +343,7 @@ impl<T: NvsPartitionId> RawStorage for EspNvs<T> {
                     ((u64value >> 48) & 0xff) as u8,
                 ];
 
-                buf.copy_from_slice(&array[..len as usize]);
+                buf[..len as usize].copy_from_slice(&array[..len as usize]);
 
                 Ok(Some(&buf[..len as usize]))
             }
