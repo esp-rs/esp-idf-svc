2a3c4e7942edfa3d9c6047a15303f958fa9a9b25
Alexander Hübener <alex.teamplayer@gmail.com>
2022-12-12 12:02:05 +0100

    make clippy happy

--- src/wifi.rs
+++ src/wifi.rs
@@ -1407,7 +1407,7 @@ impl<'d, 'a> ScanProcess<'d, 'a> {
         info!("Got wifi event: {:?}", event);
 
         if matches!(event, WifiEvent::ScanStarted | WifiEvent::ScanDone) {
-            *waitable.state.lock() = event.clone().into();
+            *waitable.state.lock() = (*event).into();
             waitable.cvar.notify_all();
         }
     }
