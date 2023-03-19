8e9032e518ba49da02e121c17c26321b31e85f6f
Erland Lewin <erland@lewin.nu>
2023-03-19 08:49:35 +0000

    Attempt to address review comments

--- examples/wifi_async.rs
+++ examples/wifi_async.rs
@@ -8,7 +8,7 @@ extern crate esp_idf_svc;
 use embedded_svc::wifi::asynch::Wifi;
 use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
 use esp_idf_hal::prelude::Peripherals;
-use esp_idf_svc::wifi::EspAsyncWifiDriver;
+use esp_idf_svc::wifi::AsyncWifiDriver;
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 use futures::executor::block_on;
 
@@ -19,15 +19,15 @@ fn main() -> anyhow::Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let sys_loop = EspSystemEventLoop::take().unwrap();
     let nvs = EspDefaultNvsPartition::take().unwrap();
-
-    let mut wifi = EspAsyncWifiDriver::new(peripherals.modem, sys_loop, Some(nvs))?;
+    let mut base_driver = WifiDriver::new(peripherals.modem, sys_loop, Some(nvs))?;
+    let mut wifi = AsyncWifiDriver::new(base_driver);
 
     block_on(do_wifi(&mut wifi))?;
 
     Ok(())
 }
 
-async fn do_wifi<'a>(wifi: &mut EspAsyncWifiDriver<'a>) -> anyhow::Result<()> {
+async fn do_wifi<'a>(wifi: &mut AsyncWifiDriver<'a>) -> anyhow::Result<()> {
     let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
         ssid: SSID.into(),
         bssid: None,
