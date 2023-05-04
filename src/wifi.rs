3a94a061f4dd20a660bf6ce6b1e4f2c9ab8db60f
Frederick <frederick@vollbrecht.cc>
2023-05-04 14:02:59 +0200

    fmt

--- src/wifi.rs
+++ src/wifi.rs
@@ -801,19 +801,19 @@ impl<'d> WifiDriver<'d> {
 
     /// Get information of AP which the ESP32 station is associated with.
     /// Useful to get the current signal strength of the AP.
-    pub fn get_ap_info(&mut self) -> Result<AccessPointInfo, EspError> {        
+    pub fn get_ap_info(&mut self) -> Result<AccessPointInfo, EspError> {
         let mut ap_info_raw: wifi_ap_record_t = wifi_ap_record_t::default();
-        // If Sta not connected throws EspError(12303) 
-        esp!(unsafe { esp_wifi_sta_get_ap_info(&mut ap_info_raw)})?;
+        // If Sta not connected throws EspError(12303)
+        esp!(unsafe { esp_wifi_sta_get_ap_info(&mut ap_info_raw) })?;
         let ap_info: AccessPointInfo = Newtype(&ap_info_raw).into();
-    
+
         debug!("AP Info: {:?}", ap_info);
         Ok(ap_info)
     }
 
     /// Set RSSI threshold below which APP will get an WifiEvent::StaBssRssiLow.
     /// rssi_threshold: threshold value in dbm between -100 to 0
-    /// 
+    ///
     /// # Example
     ///
     /// This example shows how to use it in a `async` context.
@@ -826,10 +826,10 @@ impl<'d> WifiDriver<'d> {
     /// .unwrap();
     /// wifi_driver.start().unwrap();
     /// wifi_driver.connect().unwrap();
-    /// 
+    ///
     /// let rssi_low = -40;
     /// wifi_driver.driver_mut().set_sta_rssi_low(rssi_low).unwrap();
-    /// 
+    ///
     /// // Subscribe to RSSI events.
     /// let rssi_low_signal = Arc::new(channel_bridge::notification::Notification::new());
     /// let _sub = {
@@ -842,8 +842,8 @@ impl<'d> WifiDriver<'d> {
     /// };
     ///
     /// rssi_low_signal.wait().await;
-    /// // do stuff with the information 
-    /// 
+    /// // do stuff with the information
+    ///
     /// // set_rssi_threshold() has to be called again after every StaBssRssiLow event received.
     /// ```
     pub fn set_rssi_threshold(&mut self, rssi_threshold: i8) -> Result<(), EspError> {
