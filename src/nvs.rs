568085b047a6c7ab97eabcd0362a60b28f609ab6
Niklas Schurwanz <niklas.schurwanz@stabl.com>
2023-06-26 14:07:43 +0200

    fix: potential unwanted blocking behaviour - don't hold on to initialization lock of nvs partition while erasing - add comments

--- src/nvs.rs
+++ src/nvs.rs
@@ -60,12 +60,18 @@ impl NvsDefault {
 
     fn erase() -> Result<(), EspError> {
         let mut taken = DEFAULT_TAKEN.lock();
-
         if *taken {
+            // partition is already taken either by other erase call or NvsDefault instance
             esp!(ESP_ERR_INVALID_STATE)?;
         }
+        *taken = true;
+        // drop lock and reaquire later since nvs_flash_erase can take some time
+        // depending on partition size
+        drop(taken);
 
         esp!(unsafe { nvs_flash_erase() })?;
+
+        *DEFAULT_TAKEN.lock() = false;
 
         Ok(())
     }
@@ -124,14 +130,25 @@ impl NvsCustom {
     }
 
     fn erase(partition: &str) -> Result<(), EspError> {
-        let mut registrations = NONDEFAULT_LOCKED.lock();
         let c_partition = CString::new(partition).unwrap();
 
+        let mut registrations = NONDEFAULT_LOCKED.lock();
         if registrations.contains(c_partition.as_ref()) {
+            // partition is already taken either by other erase call or NvsCustom instance
             return Err(EspError::from(ESP_ERR_INVALID_STATE).unwrap());
         }
+        // mark partition as taken
+        registrations.insert(c_partition.clone());
+        // drop registrations and reaquire later since nvs_flash_erase_partition can take some time
+        // depending on partition size
+        drop(registrations);
 
         esp!(unsafe { nvs_flash_erase_partition(c_partition.as_ptr()) })?;
+
+        // remove partition taken marker
+        let mut registrations = NONDEFAULT_LOCKED.lock();
+        registrations.remove(c_partition.as_ref());
+        drop(registrations);
 
         Ok(())
     }
