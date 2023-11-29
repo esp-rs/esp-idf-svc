b0410bf593c0952469eaf6c3b24ebe35fc1a282f
Shane Snover <ssnover95@gmail.com>
2023-11-28 21:58:06 -0700

    Construct the EspWifi with the desired Netif instead of swapping them

--- examples/wifi_static_ip.rs
+++ examples/wifi_static_ip.rs
@@ -14,7 +14,7 @@ use esp_idf_svc::ipv4::{
 };
 use esp_idf_svc::log::EspLogger;
 use esp_idf_svc::netif::{EspNetif, NetifConfiguration, NetifStack};
-use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
+use esp_idf_svc::wifi::{BlockingWifi, EspWifi, WifiDriver};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 
 use log::info;
@@ -37,8 +37,8 @@ fn main() -> anyhow::Result<()> {
     let sys_loop = EspSystemEventLoop::take()?;
     let nvs = EspDefaultNvsPartition::take()?;
 
-    let mut wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
-    configure_wifi(&mut wifi);
+    let wifi = WifiDriver::new(peripherals.modem, sys_loop.clone(), Some(nvs))?;
+    let wifi = configure_wifi(wifi)?;
 
     let mut wifi = BlockingWifi::wrap(wifi, sys_loop)?;
     connect_wifi(&mut wifi)?;
@@ -54,7 +54,32 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn configure_wifi(wifi: &mut EspWifi<'_>) -> anyhow::Result<()> {
+fn configure_wifi(wifi: WifiDriver) -> anyhow::Result<EspWifi> {
+    let netmask = GATEWAY_NETMASK.unwrap_or("24");
+    let netmask = u8::from_str(netmask)?;
+    let gateway_addr = Ipv4Addr::from_str(GATEWAY_IP)?;
+    let static_ip = Ipv4Addr::from_str(DEVICE_IP)?;
+
+    let mut wifi = EspWifi::wrap_all(
+        wifi,
+        EspNetif::new_with_conf(&NetifConfiguration {
+            ip_configuration: IpConfiguration::Client(IpClientConfiguration::Fixed(
+                IpClientSettings {
+                    ip: static_ip,
+                    subnet: Subnet {
+                        gateway: gateway_addr,
+                        mask: Mask(netmask),
+                    },
+                    // Can also be set to Ipv4Addrs if you need DNS
+                    dns: None,
+                    secondary_dns: None,
+                },
+            )),
+            ..NetifConfiguration::wifi_default_client()
+        })?,
+        EspNetif::new(NetifStack::Ap)?,
+    )?;
+
     let wifi_configuration = WifiConfiguration::Client(ClientConfiguration {
         ssid: SSID.into(),
         bssid: None,
@@ -64,37 +89,7 @@ fn configure_wifi(wifi: &mut EspWifi<'_>) -> anyhow::Result<()> {
     });
     wifi.set_configuration(&wifi_configuration)?;
 
-    let netmask = GATEWAY_NETMASK.unwrap_or("24");
-    let netmask = u8::from_str(netmask)?;
-    let gateway_addr = Ipv4Addr::from_str(GATEWAY_IP)?;
-    let static_ip = Ipv4Addr::from_str(DEVICE_IP)?;
-
-    let sta_netif_cfg = EspNetif::new_with_conf(&NetifConfiguration {
-        key: "sta0".into(),
-        description: "sta0_cfg".into(),
-        ip_configuration: IpConfiguration::Client(IpClientConfiguration::Fixed(IpClientSettings {
-            ip: static_ip,
-            subnet: Subnet {
-                gateway: gateway_addr,
-                mask: Mask(netmask),
-            },
-            // Can also be set to Ipv4Addrs if you need DNS
-            dns: None,
-            secondary_dns: None,
-        })),
-        route_priority: 0,
-        stack: NetifStack::Sta,
-        custom_mac: None,
-    })?;
-    // This is not actually used in this example, but it appears to be important that the
-    // `key` value is not the same.
-    let mut ap_netif_cfg = NetifConfiguration::wifi_default_router();
-    ap_netif_cfg.key = "ap0".into();
-    let ap_netif_cfg = EspNetif::new_with_conf(&ap_netif_cfg)?;
-
-    let (_old_sta_cfg, _old_ap_cfg) = wifi.swap_netif(sta_netif_cfg, ap_netif_cfg)?;
-
-    Ok(())
+    Ok(wifi)
 }
 
 fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
