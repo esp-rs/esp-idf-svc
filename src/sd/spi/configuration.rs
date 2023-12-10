b42de3089d36701adecb820367ede692096f70c0
Alix ANNERAUD <alix@anneraud.fr>
2023-12-10 19:51:27 +0100

    Finish to work on wrapper for sdspi_device_config_t

--- src/sd/spi/configuration.rs
+++ src/sd/spi/configuration.rs
@@ -1,8 +1,7 @@
+use core::ops::Deref;
+
 use crate::sys::*;
-use esp_idf_hal::{
-    gpio::{self, Pin},
-    spi,
-};
+use esp_idf_hal::{gpio, peripheral, spi};
 
 pub struct SpiDeviceConfiguration(sdspi_device_config_t);
 
@@ -12,14 +11,14 @@ impl From<sdspi_device_config_t> for SpiDeviceConfiguration {
     }
 }
 
-impl Into<sdspi_device_config_t> for SpiDeviceConfiguration {
-    fn into(self) -> sdspi_device_config_t {
-        self.0
+impl From<SpiDeviceConfiguration> for sdspi_device_config_t {
+    fn from(config: SpiDeviceConfiguration) -> Self {
+        config.0
     }
 }
 
-impl SpiDeviceConfiguration {
-    pub fn new() -> Self {
+impl Default for SpiDeviceConfiguration {
+    fn default() -> Self {
         Self(sdspi_device_config_t {
             host_id: 0,
             gpio_cs: 0,
@@ -28,30 +27,51 @@ impl SpiDeviceConfiguration {
             gpio_int: 0,
         })
     }
+}
 
+impl SpiDeviceConfiguration {
     pub fn set_spi<T: spi::Spi>(&mut self, _: &mut T) -> &mut Self {
         self.0.host_id = T::device();
         self
     }
 
-    pub fn set_cs_pin<T: gpio::OutputPin>(&mut self, pin: T) -> &mut Self {
-        let cs_pin = pin.downgrade_output();
-        self.0.gpio_cs = cs_pin.pin();
+    pub fn set_cs_pin(
+        &mut self,
+        pin: impl peripheral::Peripheral<P = impl gpio::OutputPin>,
+    ) -> &mut Self {
+        let peripheral_reference = pin.into_ref();
+        let pin = peripheral_reference.deref();
+        self.0.gpio_cs = pin.pin();
         self
     }
 
-    pub fn set_cd_pin<T: gpio::InputPin>(&mut self, pin: &mut T) -> &mut Self {
-        self.0.gpio_cd = pin.downgrade_input().pin();
+    pub fn set_cd_pin(
+        &mut self,
+        pin: impl peripheral::Peripheral<P = impl gpio::InputPin>,
+    ) -> &mut Self {
+        let peripheral_reference = pin.into_ref();
+        let pin = peripheral_reference.deref();
+        self.0.gpio_cd = pin.pin();
         self
     }
 
-    pub fn set_wp_pin<T: gpio::InputPin>(&mut self, pin: &mut T) -> &mut Self {
-        self.0.gpio_wp = pin.downgrade_input().pin();
+    pub fn set_wp_pin(
+        &mut self,
+        pin: impl peripheral::Peripheral<P = impl gpio::InputPin>,
+    ) -> &mut Self {
+        let peripheral_reference = pin.into_ref();
+        let pin = peripheral_reference.deref();
+        self.0.gpio_wp = pin.pin();
         self
     }
 
-    pub fn set_int_pin<T: gpio::InputPin>(&mut self, pin: &mut T) -> &mut Self {
-        self.0.gpio_int = pin.downgrade_input().pin();
+    pub fn set_int_pin(
+        &mut self,
+        pin: impl peripheral::Peripheral<P = impl gpio::InputPin>,
+    ) -> &mut Self {
+        let peripheral_reference = pin.into_ref();
+        let pin = peripheral_reference.deref();
+        self.0.gpio_int = pin.pin();
         self
     }
 }
