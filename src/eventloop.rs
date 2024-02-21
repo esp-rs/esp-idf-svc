f9257d1a6e335d0c194d35eea451c7861079db77
Marvin Gudel <github.marvin@gudel.org>
2024-02-21 13:12:37 +0100

    Revert "Rename Sender to QuitOnDrop, which it actually really is"
    This reverts commit 2e65bfd8214bde14808830c788aaff4bf0f5d3d4.

--- src/eventloop.rs
+++ src/eventloop.rs
@@ -23,7 +23,7 @@ use crate::handle::RawHandle;
 use crate::private::cstr::RawCstrs;
 use crate::private::mutex;
 use crate::private::waitable::Waitable;
-use crate::private::zerocopy::{Channel, QuitOnDrop, Receiver};
+use crate::private::zerocopy::{Channel, Receiver, Sender};
 
 #[cfg(all(feature = "alloc", esp_idf_comp_esp_timer_enabled))]
 pub use async_wait::*;
@@ -570,7 +570,7 @@ where
     {
         let (channel, receiver) = Channel::new();
 
-        let sender = QuitOnDrop::new(channel);
+        let sender = Sender::new(channel);
 
         let subscription = self.subscribe::<EspEvent, _>(move |event| {
             let mut event = unsafe { core::mem::transmute(event) };
