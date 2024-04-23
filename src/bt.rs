4ee87452432b7717b41dac676ca6a370110293f2
ivmarkov <ivan.markov@gmail.com>
2024-04-23 16:40:31 +0000

    Bluedroid BLE support

--- src/bt.rs
+++ src/bt.rs
@@ -23,7 +23,6 @@ extern crate alloc;
 pub mod a2dp;
 #[cfg(all(esp32, esp_idf_bt_classic_enabled, esp_idf_bt_a2dp_enable))]
 pub mod avrc;
-// TODO: Future
 pub mod ble;
 #[cfg(all(esp32, esp_idf_bt_classic_enabled))]
 pub mod gap;
