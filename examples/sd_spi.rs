3c0ec6d68befeb502c912da4412335698aa161a8
Alix ANNERAUD <alix@anneraud.fr>
2024-07-09 14:27:18 -0400

    Fix sd_spi example

--- examples/sd_spi.rs
+++ examples/sd_spi.rs
@@ -5,12 +5,12 @@ fn main() -> anyhow::Result<()> {
         hal::{
             gpio,
             prelude::*,
-            spi::{config::DriverConfig, Dma, SpiDriver},
+            spi::{config::DriverConfig, Dma, SpiDriver, SPI3},
         },
         log::EspLogger,
         sd::{host::SdHost, spi::SpiDevice, SdConfiguration},
     };
-    use std::{fs::File, io::Write};
+    use std::{fs::{File, read_dir}, io::{Write, Read, Seek}};
 
     esp_idf_svc::sys::link_patches();
     EspLogger::initialize_default();
@@ -18,17 +18,17 @@ fn main() -> anyhow::Result<()> {
     let peripherals = Peripherals::take()?;
     let pins = peripherals.pins;
 
-    let spi_driver = SpiDriver::new(
+    let spi_driver = SpiDriver::new::<SPI3>(
         peripherals.spi3,
+        pins.gpio18,
         pins.gpio23,
-        pins.gpio19,
-        Some(pins.gpio18),
-        &DriverConfig::default().dma(Dma::Auto(0)),
+        Some(pins.gpio19),
+        &DriverConfig::default().dma(Dma::Auto(4092)),
     )?;
 
     let spi_device = SpiDevice::new(
         spi_driver,
-        pins.gpio13,
+        pins.gpio5,
         Option::<gpio::AnyInputPin>::None,
         Option::<gpio::AnyInputPin>::None,
         Option::<gpio::AnyInputPin>::None,
@@ -38,7 +38,7 @@ fn main() -> anyhow::Result<()> {
             all(esp_idf_version_major = "5", esp_idf_version_minor = "1"),
         )))] // For ESP-IDF v5.2 and later
         Option::<bool>::None,
-    );
+    )?;
 
     let host_config = SdConfiguration::new();
 
@@ -46,11 +46,35 @@ fn main() -> anyhow::Result<()> {
 
     let fat_configuration = FatConfiguration::new();
 
-    let _fat = Fat::mount(fat_configuration, host, "/")?;
+    let _fat = Fat::mount(fat_configuration, &host, "/sdspi")?;
 
-    let mut file = File::create("/test.txt")?;
+    let content = b"Hello, world!";
 
-    file.write_all(b"Hello, world!")?;
+    {
+        let mut file = File::create("/sdspi/test.txt")?;
+
+        file.write_all(content).expect("Write failed");
+
+        file.seek(std::io::SeekFrom::Start(0)).expect("Seek failed");
+    }
+
+    {
+        let mut file = File::open("/sdspi/test.txt")?;
+
+        let mut file_content = String::new();
+
+        file.read_to_string(&mut file_content).expect("Read failed");
+
+        assert_eq!(file_content.as_bytes(), content);
+    }
+
+    {
+        let directory = read_dir("/sdspi")?;
+
+        for entry in directory {
+            log::info!("Entry: {:?}", entry?.file_name());
+        }
+    }
 
     Ok(())
 }
