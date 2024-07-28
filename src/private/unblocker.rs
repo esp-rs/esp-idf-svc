9a21b60cf9b3883d48095e11932a20b1837bb114
Aron Granberg <aron.granberg@gmail.com>
2024-07-28 16:13:43 +0200

    Avoid potential memory leak when dropping mqtt clients

--- src/private/unblocker.rs
+++ src/private/unblocker.rs
@@ -5,6 +5,7 @@ use alloc::boxed::Box;
 use alloc::sync::Arc;
 
 use esp_idf_hal::cpu::Core;
+use esp_idf_hal::sys::vTaskDelete;
 use esp_idf_hal::sys::{EspError, TaskHandle_t};
 use esp_idf_hal::task;
 use esp_idf_hal::task::asynch::Notification;
@@ -17,17 +18,22 @@ where
     T: Send + 'static,
 {
     receiver: Receiver<T>,
-    task: TaskHandle_t,
 }
-
-/// SAFETY: Unblocker uses a raw pointer in Unblocker::task. This causes rust to treat it as not Send.
-/// However, this task handle is only ever used to delete the FreeRTOS task, which is safe to do from any thread.
-unsafe impl<T: Send + 'static> Send for Unblocker<T> {}
 
 impl<T> Unblocker<T>
 where
     T: Send + 'static,
 {
+    /// Create a new Unblocker
+    ///
+    /// # Arguments
+    ///
+    /// * `task_name` - The name of the task
+    /// * `stack_size` - The size of the stack for the task
+    /// * `priority` - The priority of the task
+    /// * `pin_to_core` - The core to pin the task to, if any
+    /// * `worker` - The function to run in the task. A channel will be passed to the worker with all events.
+    ///              The channel will be closed when the Unblocker is dropped. The worker should exit when the channel is closed.
     pub fn new<F>(
         task_name: &'static CStr,
         stack_size: usize,
@@ -47,7 +53,7 @@ where
 
         let worker: *mut Box<dyn FnOnce() + Send + 'static> = Box::into_raw(worker);
 
-        let task = unsafe {
+        let _task = unsafe {
             task::create(
                 Self::work,
                 task_name,
@@ -58,10 +64,11 @@ where
             )
         }
         .inspect_err(|_| unsafe {
+            // Avoid memory leak if task creation fails
             Box::from_raw(worker);
         })?;
 
-        Ok(Self { receiver, task })
+        Ok(Self { receiver })
     }
 
     pub async fn exec_in_out(&mut self) -> Option<&mut T> {
@@ -73,10 +80,22 @@ where
     }
 
     extern "C" fn work(arg: *mut core::ffi::c_void) {
-        let worker: Box<Box<dyn FnOnce() + Send + 'static>> =
-            unsafe { Box::from_raw(arg as *mut _) };
+        {
+            let worker: Box<Box<dyn FnOnce() + Send + 'static>> =
+                unsafe { Box::from_raw(arg as *mut _) };
 
-        worker();
+            worker();
+        }
+        unsafe {
+            // FreeRTOS tasks must delete themselves, returning from the task function
+            // without deleting the task will cause a crash.
+            // This function will immediately stop running. This means we have to ensure
+            // that all memory is freed before calling vTaskDelete.
+            // Fortunately, this function does not manage any memory at this point.
+            // The scope above guarantees that all relevant variables are dropped at this point.
+            // See https://www.freertos.org/implementing-a-FreeRTOS-task.html
+            vTaskDelete(std::ptr::null_mut());
+        }
     }
 }
 
@@ -85,10 +104,7 @@ where
     T: Send + 'static,
 {
     fn drop(&mut self) {
+        // This should cause the worker task to exit
         self.receiver.done();
-
-        unsafe {
-            task::destroy(self.task);
-        }
     }
 }
