0606b97e3049b7d405c041d25a57cf2d29a391de
Anatol Ulrich <45840+spookyvision@users.noreply.github.com>
2024-11-03 02:38:59 +0100

    examples/http_ws_server.rs: fix string decoding
    a (supposed) C string was treated/decoded as Rust string. This works okay-ish, but leads to a trailing null byte (valid in UTF-8!) which e.g. `serde_json` doesn't like.

--- examples/http_ws_server.rs
+++ examples/http_ws_server.rs
@@ -169,7 +169,13 @@ fn main() -> anyhow::Result<()> {
 
         let mut buf = [0; MAX_LEN]; // Small digit buffer can go on the stack
         ws.recv(buf.as_mut())?;
-        let Ok(user_string) = str::from_utf8(&buf[..len]) else {
+        
+        let Ok(user_string) = CStr::from_bytes_until_nul(&buf[..len]) else {
+            ws.send(FrameType::Text(false), "[CStr decode Error]".as_bytes())?;
+            return Ok(());
+        };
+
+        let Ok(user_string) = user_string.to_str() else {
             ws.send(FrameType::Text(false), "[UTF-8 Error]".as_bytes())?;
             return Ok(());
         };
